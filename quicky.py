619e466ef5812287f7f127e63c920e2f521d839b
Victor Mireles-Chavez <victor.mireles-chavez@semantic-web.com>
2021-02-10 09:45:24 +0100

    separate utils file

--- quicky.py
+++ quicky.py
@@ -1,78 +1,68 @@
 import rdflib
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import SPARQLWrapper, JSON, XML
 
 from rdflib.extras.external_graph_libs import rdflib_to_networkx_multidigraph
-import networkx as nx
-import matplotlib.pyplot as plt
-from pyvis.network import Network
+
+
+from utils import *
 
 sparql_client = SPARQLWrapper("https://dbpedia.org/sparql")
 cent = "<http://dbpedia.org/resource/Claude_Shannon>"
-
-
-sparql_client.setReturnFormat(JSON)
-sparql_client.setMethod("POST")
-
-
-query = """
-SELECT ?pred ?person ?place
-WHERE  {
-   ?person a <http://schema.org/Person> .
-   """ + cent + """ ?pred ?person.
-   ?person <http://dbpedia.org/ontology/birthPlace> ?place .
-}
-
-"""
-
-sparql_client.setQuery(query)
-q_results = sparql_client.query().convert()
-people = set()
-which_predicates = dict()
-for r in q_results["results"]["bindings"]:
-    people.add(rdflib.URIRef(r["person"]["value"]))
-    which_predicates[rdflib.URIRef(r["person"]["value"])] = rdflib.URIRef(r["pred"]["value"])
-
-describe_query = "DESCRIBE "
-for p in people:
-    describe_query += p.n3()+"\n"
-
-sparql_client.setQuery(describe_query)
-result = sparql_client.query().convert()
-g = rdflib.Graph()
-for b in result['results']['bindings']:
-    o = rdflib.URIRef(b['o']['value']) if b['o']['type'] == "uri" else rdflib.Literal(b['o']['value'])
-    g.add((rdflib.URIRef(b['s']['value']),rdflib.URIRef(b['p']['value']),o))
-
-print(len(g),"triples in people around Claude Shannon")
-
 predicates_of_interest = ['http://dbpedia.org/ontology/almaMater',
                           'http://dbpedia.org/ontology/academicAdvisor',
                           'http://dbpedia.org/ontology/doctoralAdvisor',
                           'http://dbpedia.org/ontology/doctoralStudent',
                           'http://dbpedia.org/ontology/employer',
                           'http://dbpedia.org/ontology/institution']
+
+
+
+# 1. We get all people associated with Shannon and their birthplaces
+sparql_client.setMethod("POST")
+query = """
+SELECT ?pred ?person ?place ?long ?lat ?placename
+WHERE  {
+   ?person a <http://schema.org/Person> .
+   """ + cent + """ ?pred ?person.
+   ?person <http://dbpedia.org/ontology/birthPlace> ?place .
+   ?place <http://www.w3.org/2000/01/rdf-schema#label> ?placename .
+   OPTIONAL { ?place <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long } .
+   OPTIONAL { ?place <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat }.
+}
+
+"""
+
+sparql_client.setReturnFormat(JSON)
+sparql_client.setQuery(query)
+q_results = sparql_client.query().convert()
+
+
+
+people = set()
+places = dict()
+for r in q_results["results"]["bindings"]:
+    people.add(rdflib.URIRef(r["person"]["value"]))
+    if "long" in r.keys() and "lat" in r.keys():
+        lat = r["lat"]["value"]
+        long = r["long"]["value"]
+        places[r["placename"]["value"]] = (lat,long)
+
+
+describe_query = "DESCRIBE "
+for p in people:
+    describe_query += p.n3()+"\n"
+
+sparql_client.setReturnFormat(XML)
+sparql_client.setQuery(describe_query)
+g = sparql_client.query().convert()
+
+print(len(g),"triples in people around Claude Shannon")
+
+
 predicates_of_interest = [rdflib.URIRef(x) for x in predicates_of_interest]
 
-subg = [triple for triple in g if triple[1] in predicates_of_interest]
+subg = [(s,p,o) for s,p,o in g
+        if p in predicates_of_interest
+        or s==cent ]
 
-def clean(uri):
-    if type(uri) is str:
-        r = uri.strip()[1:-1].split("/")[-1]
-    else:
-        r = uri.n3()[1:-1].split("/")[-1]
-    if "#" in r:
-        r = r.split("#")[-1]
-    return r.replace("_"," ")
-
-G = nx.Graph()
-G.add_node(clean(cent))
-for s,p,o in subg:
-    G.add_node(clean(s),label=clean(s))
-    G.add_edge(clean(s), clean(o), label=clean(p))
-    if s in which_predicates.keys():
-        G.add_edge(clean(s), clean(cent))
-
-# Plot Networkx instance of RDF Graph
-nt = Network("800px", "1200px")
-nt.from_nx(G)
-nt.show("nx.html")
+plotgraph(subg, filename="people_and_institutes.html")
---
+++ utils.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
+import rdflib
+
+def clean(uri):
+    if type(uri) is str:
+        r = uri.strip()[1:-1].split("/")[-1]
+    else:
+        r = uri.n3()[1:-1].split("/")[-1]
+    if "#" in r:
+        r = r.split("#")[-1]
+    return r.replace("_"," ")
+
+def plotgraph(subg, filename="nx.html",height=900,width=1600):
+    if type(subg) in [rdflib.ConjunctiveGraph, rdflib.graph]:
+        subg = [(s,p,o) for s,p,o in g]
+
+    G = nx.Graph()
+    for s,p,o in subg:
+        G.add_node(clean(s),label=clean(s))
+        G.add_edge(clean(s), clean(o), title=clean(p))
+
+
+    # Plot Networkx instance of RDF Graph
+    nt = Network(str(height)+"px", str(width)+"px")
+    nt.from_nx(G)
+    nt.show(filename)
